f271646f75b9c7b0471cf8b6246dbf2dcd11f73a
AlexioPrado <mgprado1@my.waketech.edu>
2025-11-08 00:09:11 -0500

    continue linking buttons

---
+++ guidebook.js
@@ -0,0 +1,10 @@
+
+let boobah = document.getElementById('boobahCardGame');
+let gameFlow = document.getElementById('gameFlow');
+let takingAction = document.getElementById('takingAction');
+let characterRoles = document.getElementById('characterRoles');
+let damageTypes = document.getElementById('damageTypes');
+let attributes = document.getElementById('attributes');
+let otherRules = document.getElementById('otherRules');
+let exit = document.getElementById('exit');
+

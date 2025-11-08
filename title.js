e113b7450430cf77be390780ffc96aa12fecb277
AlexioPrado <mgprado1@my.waketech.edu>
2025-11-07 22:31:58 -0500

    created the guidbook page

--- title.js
+++ title.js
@@ -1,12 +1,14 @@
 
 let scrolldown = document.getElementById('scrollDown');
 let scrollup = document.getElementById('scrollUp');
+var audio = new Audio('audio/minecraft_click.mp3')
 
 scrolldown.onclick = function () {
     window.scrollTo({
         top: 2000,
         behavior: 'smooth'
     });
+    audio.play();
     console.log('Went to History');
 }
 
@@ -15,5 +17,6 @@ scrollup.onclick = function () {
         top: 0,
         behavior: 'smooth'
     });
+    audio.play();
     console.log('Went to Title Screen');
 }

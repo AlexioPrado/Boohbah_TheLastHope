047e40aca829ac202571572b4d19a5c1828c52d2
AlexioPrado <mgprado1@my.waketech.edu>
2025-11-09 21:32:40 -0500

    started creating css and audio

---
+++ playerCodex.js
@@ -0,0 +1,16 @@
+var click = new Audio('audio/ui/minecraft_click.mp3')
+var background_audio = new Audio('audio/background/Wii Fit - Get In Shape!.mp3');
+
+document.addEventListener('mouseover', (event) => {
+    background_audio.loop = true;
+    background_audio.volume = 0.05;
+    background_audio.play();
+});
+
+document.addEventListener("visibilitychange", () => {
+  if (document.hidden) {
+    background_audio.pause();
+  } else {
+    background_audio.play()
+  }
+});
